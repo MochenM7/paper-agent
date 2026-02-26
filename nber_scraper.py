c824a56babb9e1f4599ad6ae676c76993e4ee00b
MochenM7 <113934652+MochenM7@users.noreply.github.com>
2026-02-26 18:17:22 +0100

    Update nber_scraper.py

--- nber_scraper.py
+++ nber_scraper.py
@@ -13,7 +13,6 @@ from bs4 import BeautifulSoup
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import NBER_CONFIG, ALL_KEYWORDS, TOPICS
 
 logger = logging.getLogger(__name__)
